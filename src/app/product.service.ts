b7be2460c5910d2aa39140fbac9a5aa5d54074fe
theelizabethgrace <37443911+theelizabethgrace@users.noreply.github.com>
2019-11-06 04:02:09 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-private _albumUrl = '../assets/album.js';
+private _albumUrl = '../assets/album.json';
 getAlbum(id: number){
   return this._http.get(_albumUrl).map((response)=>
   response.json());
