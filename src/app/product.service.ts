acacc684758519fe4757e88fea51ddaae14813ab
theelizabethgrace <37443911+theelizabethgrace@users.noreply.github.com>
2019-11-09 02:28:15 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,15 +10,14 @@ export class ProductService {
 private _albumUrl = '../assets/album.json';
 private _productsUrl = '../assets/products.json';
 
-getAlbum(id: number): Observable<Album> {
-  return this._http.get(this._albumUrl).map((response)=>
-  <Album>response.json());
-}
-getProducts(): Observable<Product[]> {
-  return this._http.get(_productsUrl).map((response)=>
-  <Product[]>response.json());
-}
   constructor(private _http: Http) { }
 
-
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response=>
+    <Album>response.json());
+  }
+  getProducts(): Observable<Product[]> {
+    return this._http.get(_productsUrl).map(response=>
+    <Product[]>response.json());
+  }
 }
